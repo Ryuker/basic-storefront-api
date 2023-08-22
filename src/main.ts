2b39c9e6084cff985b8331d8bdfbd7ba6b9f5758
ryuker <lifeisagamenotaproduct@gmail.com>
2023-08-22 15:13:35 +0200

    added get request which returns all products in products.json

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,59 @@
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import * as fs from 'fs';
+import productsDefaultJSON from './data/products-default.json';
+
+// server declaration
+const fastify = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+    },
+  },
+});
+
 console.log("Hello from storefront API");
+console.log(productsDefaultJSON);
+
+//plugins
+async function userRoutes(fastify: FastifyInstance){
+  fastify.get("/", {
+    handler: async(request: FastifyRequest<{
+      Body: {
+        name: string;
+        age: number;
+      }
+    }>, reply: FastifyReply) => {
+      
+      const productsJSON = fs.readFileSync('./src/data/products.json', 'utf-8');
+      const products = JSON.parse(productsJSON);
+
+      return reply.code(201).send(productsJSON);
+    }
+  });
+
+
+}
+
+// register the plugins
+fastify.register(userRoutes, { prefix: "/" })
+
+
+// server function to run the server
+async function main() {
+  await fastify.listen({
+    port: 8080,
+    host: "0.0.0.0"
+  });
+}
+
+
+// Gracefull shutdown and restart
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, async () => {
+    await fastify.close();
+    process.exit(0);
+  })
+});
+
+// Start server
+main();
